ee5dd6b6f27e5a35c6b5793ec807a29081864d3f
rjarrin <jarrin.ricardo@gmail.com>
2024-03-29 20:34:34 -0400

    Expanded Todo class functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './style.css';
 import generateTemplate from './modules/layout.js';
+import Todo from './modules/todo.js';
 
 generateTemplate();
+
+const myTodo = new Todo('Buy groceries', 'Buy milk, eggs, and bread', new Date(), new Date(), 'High', 'Remember to buy organic milk', []);
+console.log(myTodo.title); // Should log 'Buy groceries'
+myTodo.toggleComplete();
+console.log(myTodo.status); // Should log 'complete'
--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,3 +1,5 @@
+import {isBefore, format} from 'date-fns';
+
 class Todo {
     constructor(title, description, dueDate, creationDate, priority, notes, checklist) {
         this.title = title;
@@ -23,4 +25,43 @@ class Todo {
         this.notes = newNotes || this.notes;
         this.checklist = newChecklist || this.checklist;
     }
+
+    // Check if a Todo's due date has passed
+    isOverdue() {
+        const currentDate = new Date();
+        return isBefore(this.dueDate, currentDate);
+    }
+
+    // Formats the date of a todo item into more readable format (for use in UI)
+    formatDueDate() {
+        return format(this.dueDate, "yyyy-MM-dd");
+    }
+
+    // Add a note to the todo item
+    addNote(note) {
+        this.notes.push(note);
+    }
+
+    // Remove a note from a todo item
+    removeNote(note) {
+        const index = this.notes.indexOf(note);
+        if (index > -1) {
+            this.notes.splice(index, 1);
+        }
+    }
+
+    // Add an item to the checklist of a todo item
+    addChecklistItem(item) {
+        this.checklist.push(item);
+    }
+
+    // Remove an item from the checklist of a todo item
+    removeChecklistItem(item) {
+        const index = this.checklist.indexOf(item);
+        if (index > -1) {
+            this.checklist.splice(index, 1);
+        }
+    }
 }
+
+export default Todo;
